78e78c428aa2063df9c5d56ce1f802805c5f31bb
Vlad <vladbaykin@gmail.com>
2021-06-21 23:00:25 +0300

    Spring Liquibase

--- src/test/java/ru/forum/controllers/IndexControlTest.java
+++ src/test/java/ru/forum/controllers/IndexControlTest.java
@@ -1,12 +1,20 @@
 package ru.forum.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.forum.Main;
+import ru.forum.model.Post;
+import ru.forum.model.User;
+import ru.forum.service.PostService;
+import ru.forum.service.UserService;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -18,9 +26,19 @@ public class IndexControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PostService postService;
+
+    @MockBean
+    private UserService userService;
+
     @Test
     @WithMockUser
     public void shouldReturnDefaultMessage() throws Exception {
+        List<Post> posts = List.of();
+        Mockito.when(postService.findAll()).thenReturn(posts);
+        User user = new User();
+        Mockito.when(userService.findByUsername(Mockito.any())).thenReturn(user);
         this.mockMvc.perform(get("/index"))
                 .andDo(print())
                 .andExpect(status().isOk())
